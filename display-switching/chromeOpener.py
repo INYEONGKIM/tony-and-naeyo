45e38a559b23bcf10c67cb14a4411c7ecb1fb903
inyeongkim <hacerrumbo@gmail.com>
2019-11-14 20:34:22 +0900

    update : modify chrome opener

--- display-switching/chromeOpener.py
+++ display-switching/chromeOpener.py
@@ -6,79 +6,74 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 
-if len(sys.argv) != 2:
-    print("[ERROR] Execute : python chromeOpener.py start")
+# default
+page_type = -1
 
-else:
-    page_type = sys.argv[1]
-    __import__('os').system("ls")
+chrome_options = Options()
+chrome_options.add_argument('--kiosk')
 
-    chrome_options = Options()
-    chrome_options.add_argument('--kiosk')
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
 
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+# for signal
+import signal
 
-    # for signal
-    import signal
+def signal_SIGUSR1_handler(signum, frame):
+    print("Signal switching by signum", signum)
+    global driver
+    driver.switch_to.window(window_name=driver.window_handles[0])
 
-    def signal_SIGUSR1_handler(signum, frame):
-        print("Signal switching by signum", signum)
-        global driver
-        driver.switch_to.window("main")
+def signal_SIGUSR2_handler(signum, frame):
+    print("Signal switching by signum", signum)
+    global driver
+    driver.switch_to.window(window_name=driver.window_handles[1])
 
-    def signal_SIGUSR2_handler(signum, frame):
-        print("Signal switching by signum", signum)
-        global driver
-        driver.switch_to.window("map")
+def signal_SIGUSR3_handler(signum, frame):
+    print("Signal switching by signum", signum)
+    global driver
+    driver.switch_to.window(window_name=driver.window_handles[2])
 
+signal.signal(signal.SIGUSR1, signal_SIGUSR1_handler) # mac : kill -30 {pid}
+# ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -30
 
-    def signal_SIGUSR3_handler(signum, frame):
-        print("Signal switching by signum", signum)
-        global driver
+signal.signal(signal.SIGUSR2, signal_SIGUSR2_handler) # mac : kill -31 {pid}
+# ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -31
+
+signal.signal(signal.SIGINFO, signal_SIGUSR3_handler)  # mac : kill -29 {pid}
+# ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -29
+
+while True:
+    # default
+    if page_type == -1:
+
+        driver.get("http://localhost:8080/")
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(window_name=driver.window_handles[1])
+
+        driver.get("http://localhost:8080/sample2")
+        driver.execute_script("window.open('');")
         driver.switch_to.window(window_name=driver.window_handles[2])
 
-    signal.signal(signal.SIGUSR1, signal_SIGUSR1_handler) # mac : kill -30 {pid}
-    # ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -30
+        driver.get("http://localhost:8080/sample3")
 
-    signal.signal(signal.SIGUSR2, signal_SIGUSR2_handler) # mac : kill -31 {pid}
-    # ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -31
+        # set default page
+        driver.switch_to.window(window_name=driver.window_handles[0])
 
-    signal.signal(signal.SIGINFO, signal_SIGUSR3_handler)  # mac : kill -29 {pid}
-    # ps | grep chromeOpener | awk 'NR<2{print $1}' | xargs kill -29
+    elif page_type == 0:
+        print("self switch to page number 0")
+        driver.switch_to.window(window_name=driver.window_handles[0])
 
-    while True:
-        if page_type == "start":
-            driver.execute_script(
-                "(function() { " +
-                "window.open('http://localhost:8080/', 'main');" +
-                "})();"
-            )
+    elif page_type == 1:
+        print("self switch to page number 1")
+        driver.switch_to.window(window_name=driver.window_handles[1])
 
-            driver.execute_script(
-                "(function() { " +
-                "window.open('http://localhost:8080/sample4', 'map');" +
-                "})();"
-            )
-        
-            # actions = ActionChains(driver)
-            # actions.key_down(Keys.CONTROL).send_keys('w').perform()
+    elif page_type == 2:
+        print("self switch to page number 2")
+        driver.switch_to.window(window_name=driver.window_handles[2])
 
-            driver.close()
-            driver.switch_to.window("main")
+    else:
+        print("^^ㅗ")
+        break
+    page_type = int(input())
 
-
-        elif page_type == 1:
-            print("self switch to main")
-            driver.switch_to.window("main")
-
-        elif page_type == 2:
-            print("self switch to map")
-            driver.switch_to.window("map")
-
-        else:
-            print("page_type must main or map")
-            break
-        page_type = int(input())
-
-    driver.quit()
+driver.quit()
 
