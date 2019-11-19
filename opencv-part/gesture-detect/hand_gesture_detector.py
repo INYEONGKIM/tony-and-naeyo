fd16201d27562e9f547369265b2bfd8016afd90f
inyeongkim <hacerrumbo@gmail.com>
2019-11-19 22:50:35 +0900

    update : resize the origin frame

--- opencv-part/gesture-detect/hand_gesture_detector.py
+++ opencv-part/gesture-detect/hand_gesture_detector.py
@@ -43,12 +43,12 @@ def removeFaceAra(img, cascade):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
     rects = detect(gray, cascade)
-
+    
     height, width = img.shape[:2]
-
+    
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1 - 10, 0), (x2 + 10, height), (0, 0, 0), -1)
-
+    
     return img
 
 
@@ -57,9 +57,13 @@ def removeFaceAra(img, cascade):
 thumbs_up_cnt = 0
 
 while True:
-    ret, frame = vc.read()  # Reading one image frame from webcam.
+    ret, frame = vc.read()  # Reading one image frame from webcam. 1280 X 720
     frame = cv2.flip(frame, 1)
-
+    
+    # resizing
+    frame = frame[100:, 100:1180]
+    
+    
     # removing background
     # fgmask = fgbg.apply(frame)
     #
@@ -78,10 +82,10 @@ while True:
     #         cv2.circle(frame, (centerX, centerY), 1, (0, 255, 0), 2)
     #         cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255))
     #
-
+    
     # removing face
     frame = removeFaceAra(frame, cascade=cascade)
-
+    
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Converting RGB system to HSV system.
     hl = cv2.getTrackbarPos("hue_lower", "hand")
     hu = cv2.getTrackbarPos("hue_upper", "hand")
@@ -89,24 +93,24 @@ while True:
     su = cv2.getTrackbarPos("saturation_upper", "hand")
     vl = cv2.getTrackbarPos("value_lower", "hand")
     vu = cv2.getTrackbarPos("value_upper", "hand")
-
+    
     hand_lower = np.array([hl, sl, vl])
     hand_upper = np.array([hu, su, vu])
-
+    
     mask = cv2.inRange(frame_hsv, hand_lower, hand_upper)
     ret, mask = cv2.threshold(mask, 127, 255, 0)
-
+    
     kernel = np.ones((7, 7), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     mask = cv2.bilateralFilter(mask, 5, 75,75)
-
+    
     # finding the approximate contours of all closed objects in image
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     drawing = np.zeros(frame.shape, np.uint8)
     max = 0
     ci = 0 # maximum contour idx
-
+    
     for i in range(len(contours)):
         # Finding the contour with maximum size. (hand when kept considerably closer to webcam in comparison to face.
         cnt = contours[i]
@@ -115,35 +119,35 @@ while True:
             max = area
             ci = i
 
-    cnt = contours[ci]  # cnt is the largest contour
-    epsilon = 0.25 * cv2.arcLength(cnt, True)  # Further trying to better approximate the contour by making edges sharper and using lesser number of points to approximate contour cnt.
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
-    hull = cv2.convexHull(cnt, returnPoints=True)  # Finding the convex hull of largest contour
-    cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # storing the hull points and contours in "frame" image variable(matrix).
-    cv2.drawContours(frame, [hull], 0, (0, 255, 0), 3)
+cnt = contours[ci]  # cnt is the largest contour
+epsilon = 0.25 * cv2.arcLength(cnt, True)  # Further trying to better approximate the contour by making edges sharper and using lesser number of points to approximate contour cnt.
+approx = cv2.approxPolyDP(cnt, epsilon, True)
+hull = cv2.convexHull(cnt, returnPoints=True)  # Finding the convex hull of largest contour
+cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # storing the hull points and contours in "frame" image variable(matrix).
+cv2.drawContours(frame, [hull], 0, (0, 255, 0), 3)
 
-    hull = cv2.convexHull(cnt, returnPoints=False)
-    defects = cv2.convexityDefects(cnt, hull)  # Finding the defects between cnt contour and convex hull of hand.
+hull = cv2.convexHull(cnt, returnPoints=False)
+defects = cv2.convexityDefects(cnt, hull)
 
-    count = 0  # count is keeping track of number of defect points
-    for i in range(defects.shape[0]):  # count is keeping track of number of defect points
-        s, e, f, d = defects[i, 0]
-        if d > 14000 and d < 28000:  # If normal distance between farthest point(defect) and contour is > 14000 and < 28000, it is the desired defect point.
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
-            cv2.circle(frame, far, 5, [0, 0, 255], -1)  # draw a circle/ dot at the defect point.
-            count += 1  # count is keeping track of number of defect points
-
+count = 0  # count is keeping track of number of defect points
+for i in range(defects.shape[0]):  # count is keeping track of number of defect points
+    s, e, f, d = defects[i, 0]
+    if d > 14000 and d < 28000:  # If normal distance between farthest point(defect) and contour is > 14000 and < 28000, it is the desired defect point.
+        start = tuple(cnt[s][0])
+        end = tuple(cnt[e][0])
+        far = tuple(cnt[f][0])
+        cv2.circle(frame, far, 5, [0, 0, 255], -1)  # draw a circle/ dot at the defect point.
+        count += 1
+    
     if 1 <= count <= 3:
         thumbs_up_cnt += 1
-    else:
-        thumbs_up_cnt = 0
-
+else:
+    thumbs_up_cnt = 0
+    
     # cv2.drawContours(frame,[cnt],0,(255,0,0),3)
-
+    
     font = cv2.FONT_HERSHEY_SIMPLEX
-
+    
     # Outputting "count + 1"in "frame" and displaying the output.
     cv2.putText(frame, str(count + 1), (100, 100), font, 1, (0, 0, 255), 1)
     if thumbs_up_cnt >= 3:
@@ -151,8 +155,8 @@ while True:
 
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(20) == 27:
-        break
+if cv2.waitKey(20) == 27:
+    break
 
 vc.release()
 cv2.destroyAllWindows()
